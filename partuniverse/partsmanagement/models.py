278772520c464012c0002b4fb319025b7e8d8a48
Frank Lanitz <frank@frank.uvena.de>
2014-10-11 16:06:47 +0200

    Adding a very basic draft of model structure

--- partuniverse/partsmanagement/models.py
+++ partuniverse/partsmanagement/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
 
-# Create your models here.
+class StoragePlace(models.Model):
+	'''Representing the place inside the storage'''
+	name = models.CharField(max_length=50)
+
+class Part(models.Model):
+	'''Representing a special kind of parts'''
+	name = models.CharField(max_length=50)
+
+class Categorie(models.Model):
+	'''Representing a category a part might contains to. E.g. resistor'''
+	name = models.CharField(max_length=50)
+	parent = models.ForeignKey("self", null=True, blank=True)
+
+	def __unicode__(self):
+		return self.name
+
+class Transaction(models.Model):
+	'''The transaction really taking place for the part'''
+	subject = models.CharField(max_length=100)
+	user = models.ForeignKey(User)
+	amount = models.DecimalField(max_digits=10, decimal_places=2)
+	part = models.ForeignKey(Part)
+	date = models.DateField(blank=False, null=False)
+
