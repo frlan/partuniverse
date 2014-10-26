e35e1ca8fa2f25de66993cd07e8c7551cadb929e
Frank Lanitz <frank@frank.uvena.de>
2014-10-26 20:39:15 +0100

    Fix import of views into urls.py which cause issues in some systems. Thanks to egon0 for reporting

--- partuniverse/partsmanagement/urls.py
+++ partuniverse/partsmanagement/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse
 
 
-from views import PartsList, PartsAddView
+from .views import PartsList, PartsAddView
 
 urlpatterns = patterns('',
 	url(r'^list/', PartsList.as_view(), name='partslist'),
