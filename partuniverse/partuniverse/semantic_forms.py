2831a652987d1feeab750ab0cd1202a9e467ca6d
Tim <tim@datenknoten.me>
2015-06-22 22:05:10 +0200

    Only append the hidden fields to the output.
    Since we dont have any fancy parts tables any more we can just append hidden fields to the output.
    
    Fixes #88

--- partuniverse/partuniverse/semantic_forms.py
+++ partuniverse/partuniverse/semantic_forms.py
@@ -72,23 +72,5 @@ def as_semantic(self):
     
 
     if hidden_fields:  # Insert any hidden fields in the last row.
-        str_hidden = ''.join(hidden_fields)
-        if output:
-            last_row = output[-1]
-            # Chop off the trailing row_ender (e.g. '</td></tr>') and
-            # insert the hidden fields.
-            if not last_row.endswith(row_ender):
-                # This can happen in the as_p() case (and possibly others
-                # that users write): if there are only top errors, we may
-                # not be able to conscript the last row for our purposes,
-                # so insert a new, empty row.
-                last_row = (normal_row % {'errors': '', 'label': '',
-                                          'field': '', 'help_text': '',
-                                          'html_class_attr': html_class_attr})
-                output.append(last_row)
-            output[-1] = last_row[:-len(row_ender)] + str_hidden + row_ender
-        else:
-            # If there aren't any rows in the output, just append the
-            # hidden fields.
-            output.append(str_hidden)
+        output.append(str_hidden)
     return mark_safe('\n'.join(output))
