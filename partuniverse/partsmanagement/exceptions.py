16a47a47d6d7f05ef451949e7b2ddb698cb9621e
Frank Lanitz <frank@frank.uvena.de>
2015-07-15 22:28:34 +0200

    Add logging message for generic exceptions

--- partuniverse/partsmanagement/exceptions.py
+++ partuniverse/partsmanagement/exceptions.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+#i18n (just in case)
+from django.utils.translation import ugettext_lazy as _
 
 # Logging
 import logging
@@ -10,11 +12,13 @@ class PartsmanagementException(Exception):
     """
     Base exceptions for this packages
     """
+    logger.error(_("An fatal error has been occurred"))
     pass
 
 class PartsNotFitException(PartsmanagementException):
     """
     Shall be used, when ever a Part is not fitting to another such like cannot be compared
     """
+    logger.error(_("Exception: Parts cannot be combined"))
     pass
 
