106acf13aaebfa0cc1503be9fe3ec1c1fb966791
Frank Lanitz <frank@frank.uvena.de>
2016-02-15 23:51:38 +0100

    Travis: Add database migration scripts to test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 cp partuniverse/partuniverse/local_settings.py.tpl_dev partuniverse/partuniverse/local_settings.py
+python partuniverse/manage.py makemigrations
+python partuniverse/manage.py migrate
 python partuniverse/manage.py test
