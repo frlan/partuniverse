d7ce35ba1c33167935193b9d0ca0c63de1aac00b
Frank Lanitz <frank@frank.uvena.de>
2016-04-28 15:36:10 +0200

    Add Polymorphic parent class to admin of Part

--- partuniverse/partsmanagement/admin.py
+++ partuniverse/partsmanagement/admin.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
+from bookcollection.admin import BookAdmin
+from bookcollection.models import Book
 from django.contrib import admin
 from .models import *
+from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin
 
 # Logging
 import logging
 logger = logging.getLogger(__name__)
+
+
+class PartAdmin(PolymorphicParentModelAdmin):
+    base_model = Part
+    child_models = (
+        (Book, BookAdmin)
+    )
+
 
 admin.site.register(StorageType)
 admin.site.register(Category)
