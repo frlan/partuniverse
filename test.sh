5cd88960bb618d80b27d9d85a04233a43fe71b38
Tim Schumacher <tim@datenknoten.me>
2016-06-20 19:15:45 +0200

    Use django for teh tests

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
+python bootstrap-buildout.py
+./bin/buildout
 cp partuniverse/partuniverse/local_settings.py.tpl_dev partuniverse/partuniverse/local_settings.py
-python partuniverse/manage.py makemigrations
-python partuniverse/manage.py migrate
-python partuniverse/manage.py test
+./bin/django makemigrations
+./bin/django migrate
+./bin/django test
