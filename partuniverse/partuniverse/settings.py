09ffc7ea6f7a38ccf694ac38d6a5b913ead5aa5b
Frank Lanitz <frank@lanitz.info>
2018-08-17 19:58:34 +0200

    Replace deprecated MIDDLEWARE_CLASSES in settings.py

--- partuniverse/partuniverse/settings.py
+++ partuniverse/partuniverse/settings.py
@@ -61,17 +61,15 @@ INSTALLED_APPS = (
     'partsmanagement'
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'account.middleware.LocaleMiddleware',
     'account.middleware.TimezoneMiddleware',
-)
+]
 
 ROOT_URLCONF = 'partuniverse.urls'
 
