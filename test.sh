fd5062186959450daac39f88d063dea2fe20552f
Frank Lanitz <frank@frank.uvena.de>
2018-08-21 17:03:45 +0200

    Fix typo

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-virtualenv --python=pythn3 .
+virtualenv --python=python3 .
 ./bin/pip install zc.buildout
 ./bin/buildout
 cp partuniverse/partuniverse/local_settings.py.tpl_dev partuniverse/partuniverse/local_settings.py
